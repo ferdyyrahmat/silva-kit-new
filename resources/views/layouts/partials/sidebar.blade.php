60e3d6f0ad5c0d1141c908b85208b96676805ad4
Ferdy Rahmat <ferdyyrahmat@gmail.com>
2026-07-22 21:19:22 +0700

    initialize commit

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,97 @@
+<!-- Left Sidebar Start -->
+<div class="app-sidebar-menu">
+    <div class="h-100" data-simplebar>
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+
+            <div class="logo-box">
+                <a href="{{ route('second', ['dashboard', 'index'])}}" class="logo logo-light">
+                    <span class="logo-sm">
+                        <img src="/images/logo-sm.png" alt="" height="22">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="/images/logo-light.png" alt="" height="24">
+                    </span>
+                </a>
+                <a href="{{ route('second', ['dashboard', 'index'])}}" class="logo logo-dark">
+                    <span class="logo-sm">
+                        <img src="/images/logo-sm.png" alt="" height="22">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="/images/logo-dark.png" alt="" height="24">
+                    </span>
+                </a>
+            </div>
+
+            <ul id="side-menu">
+
+                <li class="menu-title">Menu</li>
+
+                <li>
+                    <a href="{{ route('root')}}" class="tp-link">
+                        <i data-feather="home"></i>
+                        <span> Dashboard </span>
+                    </a>
+                </li>
+
+                {{-- <li>
+                    <a href="#sidebarDashboards" data-bs-toggle="collapse">
+                        <i data-feather="home"></i>
+                        <span> Dashboard </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarDashboards">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('second', ['dashboard', 'index'])}}" class="tp-link">CRM</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('second', ['dashboard', 'analytics'])}}" class="tp-link">Analytics</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('second', ['dashboard', 'ecommerce'])}}" class="tp-link">eCommerce</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li> --}}
+
+                <li class="menu-title">Pages</li>
+
+                <li>
+                    <a href="#sidebarError" data-bs-toggle="collapse">
+                        <i data-feather="alert-octagon"></i>
+                        <span> Error Pages </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarError">
+                        <ul class="nav-second-level">
+                            <li>
+                                <a href="{{ route('second', ['error', 'error-404'])}}" class="tp-link">Error 404</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('second', ['error', 'error-500'])}}" class="tp-link">Error 500</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('second', ['error', 'error-503'])}}" class="tp-link">Error 503</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('second', ['error', 'error-429'])}}" class="tp-link">Error 429</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('second', ['error', 'offline-page'])}}" class="tp-link">Offline Page</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+</div>
+<!-- Left Sidebar End -->
